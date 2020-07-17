496b6733a162529c0540213773b69d10f71fb431
OrtizIgnacioA <nacho.agus.ortiz@gmail.com>
2020-07-17 20:41:04 -0300

    0015-0001-fixture
    Se crea Fixture PacienteCreate y PacienteRecreate

---
+++ application/src/main/java/domainapp/application/fixture/scenarios/PacienteCreate.java
@@ -0,0 +1,106 @@
+package domainapp.application.fixture.scenarios;
+
+import domainapp.modules.simple.dom.impl.paciente.HabitacionSeleccionada;
+import domainapp.modules.simple.dom.impl.paciente.Paciente;
+import domainapp.modules.simple.dom.impl.paciente.PacienteMenu;
+import domainapp.modules.simple.dom.impl.paciente.TipoDocumento;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.isis.applib.fixturescripts.FixtureScript;
+import org.joda.time.LocalDate;
+
+public class PacienteCreate extends FixtureScript {
+
+    @Getter @Setter
+    private LocalDate fechaAlta;
+
+    @Getter @Setter
+    private String name;
+
+    @Getter @Setter
+    private String apellido;
+
+    @Getter @Setter
+    private Integer edad;
+
+    @Getter @Setter
+    private TipoDocumento tipoDocumento;
+
+    @Getter @Setter
+    private String nroDocumento;
+
+    @Getter @Setter
+    private LocalDate fechaNacimiento;
+
+    @Getter @Setter
+    private String lugarDeNacimiento;
+
+    @Getter @Setter
+    private Integer telefono;
+
+    @Getter @Setter
+    private String numeroDeSeguroSocial;
+
+    @Getter @Setter
+    private String incapacidad;
+
+    @Getter @Setter
+    private String observacion;
+
+    @Getter @Setter
+    private String nombreFamiliar;
+
+    @Getter @Setter
+    private String apellidoFamiliar;
+
+    @Getter @Setter
+    private String parentesco;
+
+    @Getter @Setter
+    private String numeroContacto;
+
+    @Getter @Setter
+    private String mailFamiliar;
+
+    @Getter @Setter
+    private String direccionFamiliar;
+
+    @Getter @Setter
+    private HabitacionSeleccionada habitacionSeleccionada;
+
+    @Getter @Setter
+    private Paciente pacienteObject;
+
+    @Override
+    protected void execute(final ExecutionContext ec) {
+
+        LocalDate fechaAlta = checkParam("fechaAlta", ec, LocalDate.class);
+        String name = checkParam("name", ec, String.class);
+        String apellido = checkParam("apellido", ec, String.class);
+        Integer edad = checkParam("edad", ec, Integer.class);
+        TipoDocumento tipoDocumento = checkParam("tipoDocumento", ec, TipoDocumento.class);
+        String nroDocumento = checkParam("nroDocumento", ec, String.class);
+        LocalDate fechaNacimiento = checkParam("fechaNacimiento", ec, LocalDate.class);
+        String lugarDeNacimiento = checkParam("lugarDeNacimiento", ec, String.class);
+        Integer telefono = checkParam("telefono", ec, Integer.class);
+        String numeroDeSeguroSocial = checkParam("numeroDeSeguroSocial", ec, String.class);
+        String incapacidad = checkParam("incapacidad", ec, String.class);
+        String observacion = checkParam("observacion", ec, String.class);
+        String nombreFamiliar = checkParam("nombreFamiliar", ec, String.class);
+        String apellidoFamiliar = checkParam("apellidoFamiliar", ec, String.class);
+        String parentesco = checkParam("parentesco", ec, String.class);
+        String numeroContacto = checkParam("numeroContacto", ec, String.class);
+        String mailFamiliar = checkParam("mailFamiliar", ec, String.class);
+        String direccionFamiliar = checkParam("direccionFamiliar", ec, String.class);
+        HabitacionSeleccionada habitacionSeleccionada = checkParam("habitacionSeleccionada", ec, HabitacionSeleccionada.class);
+
+        this.pacienteObject = wrap(menu).create(name,apellido,fechaAlta,edad,tipoDocumento,nroDocumento,fechaNacimiento,lugarDeNacimiento,telefono,numeroDeSeguroSocial,incapacidad,observacion,nombreFamiliar,apellidoFamiliar,parentesco,numeroContacto,mailFamiliar,direccionFamiliar,habitacionSeleccionada);
+
+        // also make available to UI
+        ec.addResult(this, pacienteObject);
+    }
+
+    @javax.inject.Inject
+    PacienteMenu menu;
+
+}
---
+++ application/src/main/java/domainapp/application/fixture/scenarios/PacienteRecreate.java
@@ -0,0 +1,62 @@
+package domainapp.application.fixture.scenarios;
+
+
+import org.apache.isis.applib.fixturescripts.FixtureScript;
+
+import org.joda.time.LocalDate;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class PacienteRecreate extends FixtureScript {
+
+    public final List<LocalDate> FachaAlta = Collections.unmodifiableList(Arrays.asList(LocalDate.parse("12/12/2020"),LocalDate.parse("12/12/2015"),LocalDate.parse("12/12/2016"),LocalDate.parse("12/12/2017"),LocalDate.parse("12/12/2018"),LocalDate.parse("12/12/2019")));
+    public final List<String> Name = Collections.unmodifiableList(Arrays.asList("Halliburton Argentina S.r.l.", "Schlumberger Neuquen", "Weatherford", "NABORS", "Meier & Fischer", "Ingeniería SIMA SA"));
+    public final List<String> Apellido = Collections.unmodifiableList(Arrays.asList("San Fernando y Tinogasta, Neuquen", "Tte. Juan Solalique 412-440, Neuquén", "Juan José Lastra 5500, Neuquén", "Dr. Teodoro Luis Planas 4955, Neuquén", "Carlos Pellegrini 2560, Neuquén", "PIN Este - Manzana R Lote M1, Neuquén"));
+    public final List<Integer> Edad = Collections.unmodifiableList(Arrays.asList(65,82,81,76,91,86));
+    public final List<String> NroDocumento = Collections.unmodifiableList(Arrays.asList("Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada"));
+    public final List<LocalDate> FechaNacimiento = Collections.unmodifiableList(Arrays.asList(LocalDate.parse("12/12/1951"),LocalDate.parse("12/12/1942"),LocalDate.parse("12/12/1954"),LocalDate.parse("12/12/1940"),LocalDate.parse("12/12/1928"),LocalDate.parse("12/12/1945")));
+    public final List<String> LugarDeNacimiento = Collections.unmodifiableList(Arrays.asList("Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada"));
+    public final List<Integer> Telefono = Collections.unmodifiableList(Arrays.asList(22,33,44,55,66,77));
+    public final List<String> NumeroDeSeguroSocial = Collections.unmodifiableList(Arrays.asList("Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada"));
+    public final List<String> Incapacidad = Collections.unmodifiableList(Arrays.asList("Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada"));
+    public final List<String> Observacion = Collections.unmodifiableList(Arrays.asList("Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada"));
+    public final List<String> NombreFamiliar = Collections.unmodifiableList(Arrays.asList("Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada"));
+    public final List<String> ApellidoFamiliar = Collections.unmodifiableList(Arrays.asList("Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada"));
+    public final List<String> Parentesco = Collections.unmodifiableList(Arrays.asList("Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada"));
+    public final List<String> NumeroContacto = Collections.unmodifiableList(Arrays.asList("Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada"));
+    public final List<String> MailFamiliar = Collections.unmodifiableList(Arrays.asList("Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada"));
+    public final List<String> DireccionFamiliar = Collections.unmodifiableList(Arrays.asList("Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada"));
+
+
+    public PacienteRecreate() {
+        withDiscoverability(Discoverability.DISCOVERABLE);
+    }
+
+    @Override
+    protected void execute(final ExecutionContext ec) {
+
+        for (int i = 0; i < Name.size(); i++) {
+            final PacienteCreate fs = new PacienteCreate();
+            fs.setFechaAlta(FachaAlta.get(i));
+            fs.setName(Name.get(i));
+            fs.setApellido(Apellido.get(i));
+            fs.setEdad(Edad.get(i));
+            fs.setNroDocumento(NroDocumento.get(i));
+            fs.setFechaNacimiento(FechaNacimiento.get(i));
+            fs.setLugarDeNacimiento(LugarDeNacimiento.get(i));
+            fs.setTelefono(Telefono.get(i));
+            fs.setNumeroDeSeguroSocial(NumeroDeSeguroSocial.get(i));
+            fs.setIncapacidad(Incapacidad.get(i));
+            fs.setObservacion(Observacion.get(i));
+            fs.setNombreFamiliar(NombreFamiliar.get(i));
+            fs.setApellidoFamiliar(ApellidoFamiliar.get(i));
+            fs.setParentesco(Parentesco.get(i));
+            fs.setNumeroContacto(NumeroContacto.get(i));
+            fs.setMailFamiliar(MailFamiliar.get(i));
+            fs.setDireccionFamiliar(DireccionFamiliar.get(i));
+
+            ec.executeChild(this, fs.getName(), fs);
+        }
+    }
+}
