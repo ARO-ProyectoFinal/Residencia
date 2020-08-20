e0a41e454ca4894fd6d8e5f0f9dd3722169798df
OrtizIgnacioA <nacho.agus.ortiz@gmail.com>
2020-08-20 11:27:44 -0300

    0015-0007-fixture-visita
    Se modifica la clase VisitaRecreate

--- application/src/main/java/domainapp/application/fixture/scenarios/VisitaRecreate.java
+++ application/src/main/java/domainapp/application/fixture/scenarios/VisitaRecreate.java
@@ -17,7 +17,6 @@ public class VisitaRecreate extends FixtureScript {
     public final List<Integer> FrecuenciaRespiratoria = Collections.unmodifiableList(Arrays.asList(65,82,81,76,91,86));
     public final List<String> EstudiosLaboratorio = Collections.unmodifiableList(Arrays.asList("Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada"));
     public final List<String> Observacion = Collections.unmodifiableList(Arrays.asList("Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada"));
-    public final List<Visita> VisitaObject = Collections.unmodifiableList(Arrays.asList("Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada", "Habilitada"));
 
     public VisitaRecreate() {
         withDiscoverability(Discoverability.DISCOVERABLE);
@@ -36,7 +35,6 @@ public class VisitaRecreate extends FixtureScript {
             fs.setFrecuenciaRespiratoria(FrecuenciaRespiratoria.get(i));
             fs.setEstudiosLaboratorio(EstudiosLaboratorio.get(i));
             fs.setObservacion(Observacion.get(i));
-            fs.setVisitaObject(VisitaObject.get(i));
 
             ec.executeChild(this, fs.getAltura(), fs);
         }
