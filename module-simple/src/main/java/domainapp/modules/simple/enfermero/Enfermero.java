e74d1c1316ff11ef62823ba541da89d5370f07ef
nico1512 <nicoassanelli@hotmail.com>
2020-08-23 21:59:34 -0300

    0008-0002-Clase-Enfermero
    Se agregan los atributos a la clase Enfermero

--- module-simple/src/main/java/domainapp/modules/simple/enfermero/Enfermero.java
+++ module-simple/src/main/java/domainapp/modules/simple/enfermero/Enfermero.java
@@ -1,4 +1,92 @@
 package domainapp.modules.simple.enfermero;
 
-public class Enfermero {
+
+import domainapp.modules.simple.paciente.TipoDocumento;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.isis.applib.annotation.*;
+
+import org.apache.isis.applib.services.message.MessageService;
+import org.apache.isis.applib.services.title.TitleService;
+import org.apache.isis.schema.utils.jaxbadapters.JodaDateTimeStringAdapter;
+import org.joda.time.LocalDate;
+
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+@Getter
+@Setter
+@lombok.RequiredArgsConstructor
+public class Enfermero  {
+
+    @javax.jdo.annotations.Column(allowsNull = "false", length = 40)
+    @lombok.NonNull
+    @Property()
+    private String nombre;
+
+    @javax.jdo.annotations.Column(allowsNull = "false", length = 40)
+    @lombok.NonNull
+    @Property()
+    private String apellido;
+
+    @javax.jdo.annotations.Column(allowsNull = "true")
+    @lombok.NonNull
+    @Property() // editing disabled by default, see isis.properties
+    @XmlJavaTypeAdapter(JodaDateTimeStringAdapter.ForJaxb.class)
+    private LocalDate fechaAlta;
+
+    @javax.jdo.annotations.Column(allowsNull = "true")
+    @lombok.NonNull
+    @Property()
+    private TipoDocumento tipoDocumento;
+
+    @javax.jdo.annotations.Column(allowsNull = "false", length = 40)
+    @lombok.NonNull
+    @Property()
+    private String nroDocumento;
+
+    @javax.jdo.annotations.Column(allowsNull = "true")
+    @lombok.NonNull
+    @Property() // editing disabled by default, see isis.properties
+    @XmlJavaTypeAdapter(JodaDateTimeStringAdapter.ForJaxb.class)
+    private LocalDate fechaNacimiento;
+
+    @javax.jdo.annotations.Column(allowsNull = "false", length = 40)
+    @lombok.NonNull
+    @Property()
+    private String lugarNacimiento;
+
+    @javax.jdo.annotations.Column(allowsNull = "false", length = 40)
+    @lombok.NonNull
+    @Property()
+    private String telefono;
+
+    @javax.jdo.annotations.Column(allowsNull = "false", length = 40)
+    @lombok.NonNull
+    @Property()
+    private String edad;
+
+    @javax.jdo.annotations.Column(allowsNull = "false", length = 40)
+    @lombok.NonNull
+    @Property()
+    private String nroMatricula;
+
+
+
+    @javax.inject.Inject
+    @javax.jdo.annotations.NotPersistent
+    @lombok.Getter(AccessLevel.NONE) @lombok.Setter(AccessLevel.NONE)
+    EnfermeroRepository enfermeroRepository;
+
+    @javax.inject.Inject
+    @javax.jdo.annotations.NotPersistent
+    @lombok.Getter(AccessLevel.NONE) @lombok.Setter(AccessLevel.NONE)
+    TitleService titleService;
+
+    @javax.inject.Inject
+    @javax.jdo.annotations.NotPersistent
+    @lombok.Getter(AccessLevel.NONE) @lombok.Setter(AccessLevel.NONE)
+    MessageService messageService;
+
+
 }
