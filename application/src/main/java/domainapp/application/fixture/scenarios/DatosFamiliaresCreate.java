f2cc14f5f263e895dbb0b9218f9c01c60e34dedb
OrtizIgnacioA <nacho.agus.ortiz@gmail.com>
2020-08-28 12:25:37 -0300

    Issue #15 Fixture
    Se crea la clase DatosFamiliaresCreate y DatosFamiliaresRecreate

---
+++ application/src/main/java/domainapp/application/fixture/scenarios/DatosFamiliaresCreate.java
@@ -0,0 +1,43 @@
+package domainapp.application.fixture.scenarios;
+
+import domainapp.modules.simple.datosFamiliares.DatosFamiliares;
+import domainapp.modules.simple.datosFamiliares.DatosFamiliaresMenu;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.isis.applib.fixturescripts.FixtureScript;
+
+public class DatosFamiliaresCreate extends FixtureScript {
+
+    @Getter @Setter
+    private String nombreCompletoFamiliar;
+
+    @Getter @Setter
+    private String parentesco;
+
+    @Getter @Setter
+    private String numeroContacto;
+
+    @Getter @Setter
+    private String mailFamiliar;
+
+    @Getter @Setter
+    private DatosFamiliares datosFamiliaresObject;
+
+    @Override
+    protected void execute(final ExecutionContext ec) {
+
+        String nombreCompletoFamiliar = checkParam("nombreCompletoFamiliar", ec, String.class);
+        String parentesco = checkParam("parentesco", ec, String.class);
+        String numeroContacto = checkParam("numeroContacto", ec, String.class);
+        String mailFamiliar = checkParam("mailFamiliar", ec, String.class);
+
+
+        this.datosFamiliaresObject = wrap(menu).create(nombreCompletoFamiliar,parentesco,numeroContacto,mailFamiliar);
+
+        // also make available to UI
+        ec.addResult(this, datosFamiliaresObject);
+    }
+
+    @javax.inject.Inject
+    DatosFamiliaresMenu menu;
+}
---
+++ application/src/main/java/domainapp/application/fixture/scenarios/DatosFamiliaresRecreate.java
@@ -0,0 +1,34 @@
+package domainapp.application.fixture.scenarios;
+
+import domainapp.modules.simple.datosFamiliares.DatosFamiliares;
+import org.apache.isis.applib.fixturescripts.FixtureScript;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class DatosFamiliaresRecreate extends FixtureScript {
+
+    public final List<String> NombreCompletoFamiliar = Collections.unmodifiableList(Arrays.asList("Carlos", "Marcos", "Andrea", "Luz", "Roberto", "Patricio"));
+    public final List<String> Narentesco = Collections.unmodifiableList(Arrays.asList("Hermano", "Hermano", "Hija", "Hermana", "Hermano", "Hijo"));
+    public final List<String> NumeroContacto = Collections.unmodifiableList(Arrays.asList("2994578632", "2993515759", "2991475852", "2993102456", "2993120123", "2999874102"));
+    public final List<String> MailFamiliar = Collections.unmodifiableList(Arrays.asList("carlos@gmail.com", "marcos@gmail.com", "andrea@gmail.com", "luz@gmail.com", "roberto@gmail.com", "patricio@gmail.com"));
+
+    public DatosFamiliaresRecreate() {
+        withDiscoverability(Discoverability.DISCOVERABLE);
+    }
+
+    @Override
+    protected void execute(final ExecutionContext ec) {
+
+        for (int i = 0; i < NombreCompletoFamiliar.size(); i++) {
+            final DatosFamiliaresCreate fs = new DatosFamiliaresCreate();
+            fs.setNombreCompletoFamiliar(NombreCompletoFamiliar.get(i));
+            fs.setParentesco(Narentesco.get(i));
+            fs.setNumeroContacto(NumeroContacto.get(i));
+            fs.setMailFamiliar(MailFamiliar.get(i));
+
+            ec.executeChild(this, fs.getNombreCompletoFamiliar(), fs);
+        }
+    }
+}
