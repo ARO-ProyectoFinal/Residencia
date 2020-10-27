4d270406b8f934388992efb9c9a42c8292fd4c8e
OrtizIgnacioA <nacho.agus.ortiz@gmail.com>
2020-10-27 20:40:29 -0300

    Issue #19 Metodos
    Se modifica metodo update de la entidad Visita Medica

--- module-simple/src/main/java/domainapp/modules/simple/visita/Visita.java
+++ module-simple/src/main/java/domainapp/modules/simple/visita/Visita.java
@@ -2,6 +2,7 @@ package domainapp.modules.simple.visita;
 
 
 import com.google.common.collect.ComparisonChain;
+import domainapp.modules.simple.datosFamiliares.DatosFamiliares;
 import domainapp.modules.simple.paciente.Paciente;
 import domainapp.modules.simple.paciente.PacienteRepository;
 import lombok.AccessLevel;
@@ -99,26 +100,83 @@ public class Visita {
 
 
 
-    @Action(semantics = IDEMPOTENT, command = ENABLED, publishing = Publishing.ENABLED, associateWith = "altura")
-    public Visita updateName(
+    @Action()
+    @ActionLayout(named = "Editar")
+    public Visita updateVisita(
             @Parameter(maxLength = 40)
-            @ParameterLayout(named = "altura") final String altura
+            @ParameterLayout(named = "Altura")
+            final String altura,
+
+            @Parameter(maxLength = 40)
+            @ParameterLayout(named = "Peso")
+            final String peso,
+
+            @Parameter(maxLength = 40)
+            @ParameterLayout(named = "Temperatura")
+            final String temperatura,
+
+            @Parameter(maxLength = 40)
+            @ParameterLayout(named = "Presion Arterial")
+            final String presionArterial,
+
+            @Parameter(maxLength = 40)
+            @ParameterLayout(named = "Frecuencia Cardiaca")
+            final String frecuenciaCardiaca,
+
+            @Parameter(maxLength = 40)
+            @ParameterLayout(named = "Frecuencia Respiratoria")
+            final String frecuenciaRespiratoria,
+
+            @Parameter(maxLength = 40)
+            @ParameterLayout(named = "Estudios Laboratorio")
+            final String estudiosLaboratorio,
+
+            @Parameter(maxLength = 40)
+            @ParameterLayout(named = "Observacion")
+            final String observacion){
+
+            this.altura = altura;
+            this.peso = peso;
+            this.temperatura = temperatura;
+            this.presionArterial = presionArterial;
+            this.frecuenciaCardiaca = frecuenciaCardiaca;
+            this.frecuenciaRespiratoria = frecuenciaRespiratoria;
+            this.estudiosLaboratorio = estudiosLaboratorio;
+            this.observacion = observacion;
 
 
-    ) {
-        setAltura(altura);
-
-        return this;
+            return this;
 
     }
 
-    public String default0UpdateName() {
+    public String default0UpdateVisita() {
         return getAltura();
     }
-
-    public TranslatableString validate0UpdateName(final String altura) {
-        return altura != null && altura.contains("!") ? TranslatableString.tr("Exclamation mark is not allowed") : null;
+    public String default1UpdateVisita() {
+        return getPeso();
     }
+    public String default2UpdateVisita() {
+        return getTemperatura();
+    }
+    public String default3UpdateVisita() {
+        return getPresionArterial();
+    }
+    public String default4UpdateVisita() {
+        return getFrecuenciaCardiaca();
+    }
+    public String default5UpdateVisita() {
+        return getFrecuenciaRespiratoria();
+    }
+    public String default6UpdateVisita() {
+        return getEstudiosLaboratorio();
+    }
+    public String default7UpdateVisita() {
+        return getObservacion();
+    }
+
+    /*public TranslatableString validate0UpdateName(final String altura) {
+        return altura != null && altura.contains("!") ? TranslatableString.tr("Exclamation mark is not allowed") : null;
+    }*/
 
 
     // @Action(semantics = NON_IDEMPOTENT_ARE_YOU_SURE)
