5beac45b0c6f48f650037cc03333cde717bc2577
OrtizIgnacioA <nacho.agus.ortiz@gmail.com>
2021-01-13 11:32:06 -0300

    Issue #22 Crear componentes
    Se modifica el componente home para agregar sliders

--- ARO-App/src/app/home/home.page.ts
+++ ARO-App/src/app/home/home.page.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  slideOpt = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    centeredSlides: true,
+    speed: 400,
+  };
   constructor() {}
-
 }
