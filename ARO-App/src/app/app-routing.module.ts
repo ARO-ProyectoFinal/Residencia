ba138d342a0187acac8df953f50582640724dbc4
OrtizIgnacioA <nacho.agus.ortiz@gmail.com>
2021-01-14 20:34:44 -0300

    Issue #22 Componentes
    Se agrega guards para el componente login

--- ARO-App/src/app/app-routing.module.ts
+++ ARO-App/src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { IntroGuard } from './guards/intro.guard';
+import { LoginGuard } from './guards/login.guard';
 
 const routes: Routes = [
   {
     path: 'home',
     loadChildren: () =>
       import('./home/home.module').then((m) => m.HomePageModule),
-    canActivate: [IntroGuard],
+    canActivate: [LoginGuard, IntroGuard],
   },
   {
     path: '',
@@ -18,11 +19,12 @@ const routes: Routes = [
     path: 'intro',
     loadChildren: () =>
       import('./intro/intro.module').then((m) => m.IntroPageModule),
-  },  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
-
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.module').then((m) => m.LoginPageModule),
+  },
 ];
 
 @NgModule({
